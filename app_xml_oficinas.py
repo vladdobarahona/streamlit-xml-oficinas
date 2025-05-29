1531decc4b3734147d18c506ec71462e8c843676
vladdobarahona <details_quiconque.7j@icloud.com>
2025-05-29 15:22:16 -0500

    Update app_xml_oficinas.py
    Set divipola file by default

--- app_xml_oficinas.py
+++ app_xml_oficinas.py
@@ -10,7 +10,9 @@ st.title("Generador de XML de Oficinas")
 
 # Subida de archivos
 oficinas_file = st.file_uploader("Sube el archivo de oficinas (Excel)", type=["xlsx"])
-divipola_file = st.file_uploader("Sube el archivo DIVIPOLA (Excel)", type=["xlsx"])
+#divipola_file = st.file_uploader("Sube el archivo DIVIPOLA (Excel)", type=["xlsx"])
+divipola = pd.read_excel("divipola.xlsx", sheet_name='Sheet1', engine="openpyxl", dtype=str)
+
 
 if oficinas_file and divipola_file:
     if st.button("Generar XML"):
